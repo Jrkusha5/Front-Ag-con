7ea268821692ef76b2e201baa7014fb5d25bc7ec
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-05-25 13:34:38 -0700

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,12 @@ import { Link, useNavigate } from 'react-router-dom';
 import logo1 from '../assets/img/logo1.png';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCartTotal } from '../redux/cartSlice';
-import { AuthContext } from '../../context/AuthContext';
-
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext); // Destructure logout from AuthContext
   const dispatch = useDispatch();
   const { totalItems } = useSelector((state) => state.cart);
-  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,12 +16,21 @@ const Header = () => {
   }, [dispatch]);
 
   const [showMenu, setShowMenu] = useState(false);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const handleToggle = () => {
     setShowMenu(!showMenu);
   };
 
- 
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleLogout = () => {
+    logout(); // Call the logout function from AuthContext
+    navigate('/'); // Navigate to the home page
+  };
+
   return (
     <div className="container-fluid fixed-top pt-4 wow fadeIn" data-wow-delay="0.1s">
       <nav
@@ -58,18 +65,35 @@ const Header = () => {
             <Link to="/contactUs" className="nav-item nav-link">
               Contact
             </Link>
-          <Link to="/login" className="nav-item nav-link">
-             Login
-          </Link>
-
-
+            {!user && (
+              <Link to="/login" className="nav-item nav-link">
+                Login
+              </Link>
+            )}
           </div>
 
           <Link to="/Cart" className="btn-cart btn-md-square btn btn-black bg-white rounded-pill ms-4 d-lg-inline-flex">
             <i className="fa fa-shopping-cart"></i>
             <span className="btn-sm rounded-circle btn-danger d-lg-inline-block">{totalItems}</span>
           </Link>
-          <div className="d-none d-lg-flex ms-4 justify-content-end"></div>
+          {user && (
+            <div className="profileMenu ms-4 d-flex align-items-center position-relative">
+              <img 
+                src='https://i.ibb.co/4pDNDk1/avatar.png' 
+                alt="" 
+                className="topbarImg" 
+                onClick={toggleDropdown}
+                style={{ cursor: 'pointer', width: '40px', height: '40px', borderRadius: '50%' }}
+              />
+              {dropdownVisible && (
+                <div className="dropdownMenu position-absolute mt-2" style={{ top: '100%', right: 0, background: 'white', border: '1px solid #ccc', borderRadius: '5px', zIndex: 1000 }}>
+                  <Link to={`/setting/${user._id}`} className="dropdownItem" style={{ display: 'block', padding: '10px' }}>Profile</Link>
+                  <Link to='/' className="dropdownItem" style={{ display: 'block', padding: '10px' }}>Dashboard</Link>
+                  <span className="dropdownItem" onClick={handleLogout} style={{ display: 'block', padding: '10px', cursor: 'pointer' }}>LogOut</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </nav>
     </div>
