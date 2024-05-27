488e31bd2c567620391d3e82d74405866e780463
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-05-27 06:16:39 -0700

    first commit

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -11,6 +11,7 @@ import Buyer from '../pages/SignIn/Buyer'
 import Logistics from '../pages/SignIn/Logistics'
 import OrderPage from '../components/Order';
 import Blogs from '../components/BlogList';
+import Profile from '../pages/Profile/Profile'
 import {Routes, Route } from 'react-router-dom';
 //import ProtectedRoute from './ProtectedRoutes';
 // import MyAccount from '../Dashboard/buyer-account/MyAccount';
@@ -31,10 +32,8 @@ const Routers = () => {
     <Route path='/signUpLogistics' element={<Logistics/>}/>
     <Route path="/order" element={<OrderPage/>}/>
     <Route path="/blogs" element={<Blogs/>}/>
+    <Route path="/setting/:id}" element={<Profile/>}/>
 
-
-    {/* <Route path="/buyer-profile" element={<ProtectedRoute allowedRoles={['buyer']}> <MyAccount/></ProtectedRoute>}/>
-    <Route path="/farmer-profile" element={<ProtectedRoute allowedRoles={['farmer']}> <Dashboard/> </ProtectedRoute>}/> */}
    </Routes>
   )
 }
