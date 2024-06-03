b2e0bfda2de85e391f56d4f69398775f7acb231d
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-06-03 10:28:50 -0700

    first commit

--- src/components/mark/Marquee.jsx
+++ src/components/mark/Marquee.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 import img1 from '../../assets/img/one.jpg';
-import img2 from '../../assets/img/two.jpg';
+import img2 from '../../assets/img/img2.jpg';
 import img3 from '../../assets/img/three.jpg';
 import img4 from '../../assets/img/four.jpg';
+import img5 from '../../assets/img/img5.jpg'
 import '../mark/marquee.css';
 
 function App() {
   return (
     <div className="App">
       <div className="title">
-        <h1>Our Technology Partner</h1>
+        <h1>Our Partners</h1>
       </div>
 
       <div>
-        <Marquee direction="right" speed={100} delay={5} gradient={false}>
+        <Marquee direction="right" speed={100} delay={3} gradient={false}>
           <div className="image_wrapper">
             <img src={img1} alt="Technology Partner 1" />
           </div>
@@ -26,6 +27,9 @@ function App() {
           </div>
           <div className="image_wrapper">
             <img src={img4} alt="Technology Partner 4" />
+          </div>
+          <div className="image_wrapper">
+            <img src={img5} alt="Technology Partner 4" />
           </div>
         </Marquee>
       </div>
